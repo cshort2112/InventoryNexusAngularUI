8fa137c8e7d0d3d0af7cc1f317c6031b043aa4e1
cshort2112 <collin@webment.net>
2026-02-27 14:49:19 -0700

    feat(modules): add additional modules
    add additional modules with routing

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,19 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {from} from 'rxjs';
+import { HeaderComponent } from './header/header';
+import { Navbar } from './navbar/navbar';
+import { Orders } from './orders/orders';
+import { OrdersItems } from './orders-items/orders-items';
+import { RouterLink } from '@angular/router';
+import { Landing } from './landing/landing';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, Navbar, Orders, OrdersItems, Landing ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('untitled');
+  protected readonly title = signal('ShopifyIntegration');
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { Orders } from './app/orders/orders';
+
+const routes = [
+  { path: '/', component: App },
+  { path: '/orders', component: Orders }
+];
+
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
