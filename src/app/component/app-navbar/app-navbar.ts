d593cc9fd26f72369ca844aad5073425ff55af63
cshort2112 <collin@webment.net>
2026-03-03 13:02:49 -0700

    feat(navbar): add logo
    add logo with a link back to landing page

--- src/app/component/app-navbar/app-navbar.ts
+++ src/app/component/app-navbar/app-navbar.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {RouterLink, RouterLinkActive} from '@angular/router';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   imports: [
     RouterLink,
     RouterLinkActive,
+    NgOptimizedImage
   ],
   templateUrl: './app-navbar.html',
   styleUrl: './app-navbar.css',
