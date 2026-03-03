0486d9e875f232c24e8586a8892c6762f50eac21
cshort2112 <collin@webment.net>
2026-03-03 11:23:21 -0700

    feat(navbar): add a mega menu with .active css
    mark the navbar that is the current page as underlined and add a mega menu for orders

--- src/app/component/app-navbar/app-navbar.ts
+++ src/app/component/app-navbar/app-navbar.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import {RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   imports: [
     RouterLink,
+    RouterLinkActive,
   ],
   templateUrl: './app-navbar.html',
   styleUrl: './app-navbar.css',
