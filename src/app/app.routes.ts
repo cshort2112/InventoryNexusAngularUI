4142b7ef5564b43c9ce14a4ae5b1a8b00ef00d11
cshort2112 <collin@webment.net>
2026-03-04 20:03:34 -0700

    feat(contact): complete html and css of contact component
    fill in and style the contact component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,11 +5,13 @@ import {Login} from './component/login/login';
 import {About} from './component/about/about';
 import {Dashboard} from './component/dashboard/dashboard';
 import {authGuard} from './routeguards/auth.routeguard';
+import {Contact} from './component/contact/contact';
 
 export const routes: Routes = [
   { path: '', component: Landing},
   { path: 'orders', component: Orders, canActivate: [authGuard]},
   { path: 'login', component: Login },
   { path: 'about', component: About},
+  { path: 'contact', component: Contact},
   { path: 'dashboard', component: Dashboard, canActivate: [authGuard]}
 ];
